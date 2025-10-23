f63ea6c06d4e6c58101a0cb94b74100907c46277
LandfillHeart <kimbleredward@gmail.com>
2025-10-23 15:37:42 +0200

    Infrastruttura IRepository
    - IRepository per Dependency Injection
    - InMemoryProductRepository come Singleton per accesso univoco ai dati in memoria

---
+++ OrderHub/Infrastructure/InMemoryProductRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderHub.Infrastructure
+{
+	internal class InMemoryProductRepository : IProductRepository
+	{
+		// TODO: await domain contents to then couple with either types or interfaces
+		public Dictionary<int, string> ProductsByID { get; private set; }
+
+		public void CreateProduct(string productName, float productPrice)
+		{
+			// TODO: add a result message 
+			if (!IsValueValid(productName) || !IsValueValid(productPrice)) return;
+
+			ProductsByID.Add(ID_Helper.GetNewID(), productName);
+		}
+
+		public string ReadProduct(int id)
+		{
+			if (!ProductsByID.ContainsKey(id))
+			{
+				throw new ArgumentOutOfRangeException("ID non trovato");
+			}
+			return ProductsByID[id];
+		}
+
+		public void RemoveProduct(int id)
+		{
+			if (!ProductsByID.ContainsKey(id))
+			{
+				throw new ArgumentOutOfRangeException("ID non trovato");
+			}
+
+			ProductsByID.Remove(id);
+		}
+
+		public void UpdateProduct(int id, string newName, float newPrice)
+		{
+			// TODO: add a result message
+			if (!ProductsByID.ContainsKey(id))
+			{
+				throw new ArgumentOutOfRangeException("ID non trovato");
+			}
+
+			if (!IsValueValid(newName) || !IsValueValid(newPrice)) return;
+
+			ProductsByID[id] = newName;
+		}
+
+		private bool IsValueValid(string name)
+		{
+			return !string.IsNullOrEmpty(name);
+		}
+
+		private bool IsValueValid(float price)
+		{
+			return price > 0f;
+		}
+	}
+}
