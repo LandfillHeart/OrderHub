fdc76fa4defb3d159f757b5f31105f8955ed58a4
LandfillHeart <kimbleredward@gmail.com>
2025-10-23 17:37:20 +0200

    Aggiustati errori di compilazione

--- OrderHub/Application/Application.cs
+++ OrderHub/Application/Application.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain;
+using OrderHub.Application;
 
 namespace Application
 {
