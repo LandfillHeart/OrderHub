2541055231336db517bbeb0fc3a3a1a21196b37f
Marco Tassone <tassonemarco67@gmail.com>
2025-10-23 16:06:19 +0200

    Delegate completato

--- OrderHub/Program.cs
+++ OrderHub/Program.cs
@@ -1,7 +1,22 @@
-﻿public class Program
+﻿using OrderHub;
+
+public class Program
 {
 	public static void Main()
 	{
+		ILogger logger = new ConsoleLogger();
 
+		var service = new OrderService(logger);
+
+		Console.WriteLine($"inserisci email");
+		string? email = Console.ReadLine();
+		Console.WriteLine($"Inserisci un importo");
+		decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+
+		service.OnOrderPaid += (orderId, total) => logger.Log($"Order {orderId} paid for {total}");
+		service.OnOrderShipped += (orderId, total) => logger.Log($"Order {orderId} shipped to {email} for {total}");
+		service.OnOrderCancelled += (orderId, total) => logger.Log($"Order {orderId} cancelled for {total}");
+		service.EmailNotifier(email, amount);
 	}
 }
