80c32d2ac80430304563b944596af59e33a76f71
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-23 16:05:22 +0200

    Lazy

--- OrderHub/Infrastructure/Infrastructure.cs
+++ OrderHub/Infrastructure/Infrastructure.cs
@@ -2,7 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Infrastructure
 {
+    sealed class ConfigurationProvider
+    {
+        // Lazy + thread-safe
+        private static readonly Lazy<ConfigurationProvider> _lazy =
+            new Lazy<ConfigurationProvider>(() => new ConfigurationProvider());
+        public static ConfigurationProvider Instance => _lazy.Value;
+        public double TaxRate { get; private set; }
+        public string Currency { get; private set; }
+        public string Environment { get; private set; }
+        public string ApiBaseUrl { get; private set; }
+        public string CompanyName { get; private set; }
+        public DateTime LoadedAt { get; private set; }
+        public bool IsProduction{ get; private set; }
+
+        private ConfigurationProvider()
+        {
+            Currency = "EUR";
+            TaxRate = Currency == "EUR" ? 0.22 : 0.10;
+            Environment = "Demo";
+            ApiBaseUrl = "https://api.orderhub.local";
+            CompanyName = "OrderHub S.r.l.";
+            LoadedAt = DateTime.UtcNow; //UtcNow sono le coordinate attuali configurate nel pc
+            IsProduction = Environment == "Demo" ? false : true;
+        }
+
+    }
 }
